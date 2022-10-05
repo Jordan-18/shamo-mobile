646ad060668b03c0ff8f96e29dd05394fab11c30
Jordan-18 <jordanistiqlal@unida.gontor.ac.id>
2022-10-05 18:46:07 +0700

    checkpoint 20221005

--- lib/pages/checkout_page.dart
+++ lib/pages/checkout_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:shamo/theme.dart';
+import 'package:shamo/widget/checkout_cart.dart';
 
 class CheckoutPage extends StatelessWidget {
   const CheckoutPage({Key? key}) : super(key: key);
@@ -8,12 +9,251 @@ class CheckoutPage extends StatelessWidget {
   Widget build(BuildContext context) {
 
     Widget header(){
-      return AppBar();
+      return AppBar(
+        backgroundColor: backgroundColor1,
+        elevation: 0,
+        centerTitle: true,
+        title: Text(
+          'Checkout Details', 
+        ),
+      );
+    }
+
+    Widget content(){
+      return ListView(
+        padding: EdgeInsets.symmetric(
+          horizontal: defaultMargin,
+        ),
+        children: [
+          // NOTE : LIST ITEMS
+          Container(
+            margin: EdgeInsets.only(top: defaultMargin),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  'List Items',
+                  style: primaryTextStyle.copyWith(
+                    fontSize: 16,
+                    fontWeight: medium
+                  ),
+                ),
+                CheckoutCard(),
+                CheckoutCard(),
+              ],
+            ),
+          ),
+
+          // NOTE : ADDRESS DETAILS
+          Container(
+            margin: EdgeInsets.only(
+              top: defaultMargin,
+            ),
+            padding: EdgeInsets.all(20),
+            decoration: BoxDecoration(
+              color: backgroundColor4,
+              borderRadius: BorderRadius.circular(12)
+            ),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  'Address Details',
+                  style: primaryTextStyle.copyWith(
+                    fontSize: 16,
+                    fontWeight: medium,
+                  ),
+                ),
+                SizedBox(height: 12,),
+                Row(
+                  children: [
+                    Column(
+                      children: [
+                        Image.asset(
+                          'assets/store_location.png',
+                          width: 40,
+                          ), 
+                        Image.asset(
+                          'assets/line.png',
+                          height: 30,
+                          ), 
+                        Image.asset(
+                          'assets/icon_location.png',
+                          height: 40,
+                          ), 
+                      ],
+                    ),
+
+                    Column(
+                      children: [
+                        SizedBox(width: 12,),
+                      ],
+                    ),
+
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          'Store Location',
+                          style: secondaryTextStyle.copyWith(
+                            fontSize: 12,
+                            fontWeight: light
+                          ),
+                        ),
+                        Text(
+                          'Address Core',
+                          style: primaryTextStyle.copyWith(
+                            fontWeight: medium,
+                          ),
+                        ),
+
+                        SizedBox(height: defaultMargin,),
+
+                        Text(
+                          'Your Address',
+                          style: secondaryTextStyle.copyWith(
+                            fontSize: 12,
+                            fontWeight: light
+                          ),
+                        ),
+                        Text(
+                          'Surabaya City',
+                          style: primaryTextStyle.copyWith(
+                            fontWeight: medium,
+                          ),
+                        ),
+                      ],
+                    )
+                    
+                  ],
+                )
+              ],
+            ),
+          ),
+
+          // NOTE : PAYMENT SUMMARY
+          Container(
+            margin: EdgeInsets.only(
+              top: defaultMargin,
+            ),
+            padding: EdgeInsets.all(20),
+            decoration: BoxDecoration(
+              color: backgroundColor4,
+              borderRadius: BorderRadius.circular(12),
+            ),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  'Payment Summary',
+                  style: primaryTextStyle.copyWith(
+                    fontSize: 16,
+                    fontWeight: medium,
+                  ),
+                ),
+                SizedBox(height: 12,),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text(
+                      'Product Quantity',
+                      style: secondaryTextStyle.copyWith(
+                        fontSize: 12,
+                      ),
+                    ),
+                    Text(
+                      '2 Items',
+                      style: primaryTextStyle.copyWith(
+                        fontWeight: medium,
+                      ),
+                    )
+                  ],
+                ),
+                
+                SizedBox(height: 12,),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text(
+                      'Product Price',
+                      style: secondaryTextStyle.copyWith(
+                        fontSize: 12,
+                      ),
+                    ),
+                    Text(
+                      'Rp. 300.000',
+                      style: primaryTextStyle.copyWith(
+                        fontWeight: medium,
+                      ),
+                    )
+                  ],
+                ),
+
+                SizedBox(height: 12,),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text(
+                      'Shipping',
+                      style: secondaryTextStyle.copyWith(
+                        fontSize: 12,
+                      ),
+                    ),
+                    Text(
+                      'Free',
+                      style: primaryTextStyle.copyWith(
+                        fontWeight: medium,
+                      ),
+                    )
+                  ],
+                ),
+
+                SizedBox(height: 12,),
+                Divider(
+                  thickness: 0.5,
+                  color: Color(0xff2E3141),
+                ),
+                SizedBox(height: 10,),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text(
+                      'Total',
+                      style: priceTextStyle.copyWith(
+                        fontSize: 14,
+                        fontWeight: semibold,
+                      ),
+                    ),
+                    Text(
+                      'Rp. 300.000',
+                      style: priceTextStyle.copyWith(
+                        fontSize: 14,
+                        fontWeight: semibold,
+                      ),
+                    )
+                  ],
+                ),
+
+              ],
+            ),
+
+            
+          ),
+        ],
+      );
     }
 
     return Scaffold(
       backgroundColor: backgroundColor3,
-      appBar: AppBar(),
+      appBar: AppBar(
+        backgroundColor: backgroundColor1,
+        elevation: 0,
+        centerTitle: true,
+        title: Text(
+          'Checkout Details', 
+        ),
+      ),
+      body: content(),
     );
   }
 }
