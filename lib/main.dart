e8ca7e065ab8c0b36d1389c105e793b7261ea7c1
Jordan-18 <jordanistiqlal@unida.gontor.ac.id>
2022-10-26 18:55:21 +0700

    checkpoint 2022 10 26

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import 'package:shamo/pages/cart_page.dart';
 import 'package:shamo/pages/checkout_page.dart';
 import 'package:shamo/pages/checkout_success_page.dart';
@@ -9,6 +10,7 @@ import 'package:shamo/pages/product_page.dart';
 import 'package:shamo/pages/sign_in_page.dart';
 import 'package:shamo/pages/sign_up_page.dart';
 import 'package:shamo/pages/splash_page.dart';
+import 'package:shamo/providers/auth_provider.dart';
 import 'package:shamo/theme.dart';
 
 void main() => runApp(MyApp());
@@ -18,20 +20,27 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      routes: {
-        '/'                         : (context) => SplashPage(),
-        '/sign-in'                  : (context) => SignInPage(),
-        '/sign-up'                  : (context) => SignupPage(),
-        '/home'                     : (context) => MainPage(),
-        '/detail-chat'              : (context) => DetailChatPage(),
-        '/edit-profile'             : (context) => EditProfilPage(),
-        '/product'                  : (context) => Productpage(),
-        '/cart'                     : (context) => CartPage(),
-        '/checkout'                 : (context) => CheckoutPage(),
-        '/checkout-success'         : (context) => CheckoutSuccessPage(),
-      },
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(
+          create: (context) => AuthProvider(),
+          ),
+      ],
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        routes: {
+          '/'                         : (context) => SplashPage(),
+          '/sign-in'                  : (context) => SignInPage(),
+          '/sign-up'                  : (context) => SignupPage(),
+          '/home'                     : (context) => MainPage(),
+          '/detail-chat'              : (context) => DetailChatPage(),
+          '/edit-profile'             : (context) => EditProfilPage(),
+          '/product'                  : (context) => Productpage(),
+          '/cart'                     : (context) => CartPage(),
+          '/checkout'                 : (context) => CheckoutPage(),
+          '/checkout-success'         : (context) => CheckoutSuccessPage(),
+        },
+      ),
     );
   }
 }
